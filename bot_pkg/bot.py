091b35f95f5a7b58f5e5731c81b734c96b24cba9
Federico Fogolin <fede9726@gmail.com>
2020-03-04 18:38:34 +0100

    Calendar API

--- bot_pkg/bot.py
+++ bot_pkg/bot.py
@@ -1,17 +1,21 @@
-from Google import event_nextday 
+from calendar import cal_setup
+
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello I'm a bot, talk to me!!!")
 
+
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
+
 def calendar(update, context):
-    events = get_events()
+    events = cal_setup.get_calendar_service()
     res = ""
     for ev in events:
         tm = ev[0].strftime("%H:%M") + "-" + ev[2].strftime("%H:%M")
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from bot_pkg import bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+from bot_pkg import bot
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-updater = Updater(token="1136028020:AAFV5w0C5XddIuvFFTLOMKvH4jcVo-KJ7fE", use_context = True)
-dispatcher = updater.dispatcher
+TOKEN = "1136028020:AAFV5w0C5XddIuvFFTLOMKvH4jcVo-KJ7fE"
 
-start_handler = CommandHandler('start',bot.start)
-caps_handler = CommandHandler('caps', bot.caps)
-echo_handler = MessageHandler(Filters.text, bot.echo)
-calendar_handler = CommandHandler('calendar', bot.calendar)
 
-dispatcher.add_handler(calendar_handler)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(caps_handler)
-dispatcher.add_handler(echo_handler)
-updater.start_polling()
+def run_bot():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', bot.start))
+    dispatcher.add_handler(CommandHandler('caps', bot.caps))
+    dispatcher.add_handler(MessageHandler(Filters.text, bot.echo))
+    dispatcher.add_handler(CommandHandler('calendar', bot.calendar))
+
+    updater.start_polling()
+    updater.idle()
+    print("telegram bot done")
+
+
+if __name__ == "__main__":
+    run_bot()
